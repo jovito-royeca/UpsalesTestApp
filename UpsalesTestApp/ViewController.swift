2f460d159a9efa77bd0131c5a37f300a4663d755
Jovito Royeca <jovit.royeca@gmail.com>
2017-02-11 14:52:31 +0800

    feat: Created Xcode project

---
+++ UpsalesTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UpsalesTestApp
+//
+//  Created by Jovito Royeca on 11/02/2017.
+//  Copyright © 2017 Jovito Royeca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
