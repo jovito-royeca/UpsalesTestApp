cf26b3f3cb6d3171e8736d64d27efcddf6ff8e31
Jovito Royeca <jovit.royeca@gmail.com>
2017-02-28 11:26:44 +0800

    Added address annotation to the map

--- UpsalesTestApp/AccountLocationViewController.swift
+++ UpsalesTestApp/AccountLocationViewController.swift
@@ -15,7 +15,6 @@ class AccountLocationViewController: UIViewController {
     
     // MARK: Outlets
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     
     // MARK: Overrides
@@ -23,62 +22,105 @@ class AccountLocationViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        if let account = account {
-            let address = addressOf(account)
+        
+        // let us reload the last saved location
+        if let lastLocation = UserDefaults.standard.object(forKey: "lastLocation") as? [String: Any] {
+            let center = CLLocationCoordinate2D(latitude: lastLocation["lat"] as! CLLocationDegrees, longitude: lastLocation["lng"] as! CLLocationDegrees)
+            let span = MKCoordinateSpan(latitudeDelta: center.latitude / 10, longitudeDelta: center.longitude / 10)
+            let region = MKCoordinateRegion(center: center, span: span)
             
+            self.mapView.setRegion(region, animated: true)
+        }
+        
+        if let account = account {
             nameLabel.text = account.name
-            addressLabel.text = address
-            setMapLocationForAddress(address: address)
+            
+            if let address = addressOf(account) {
+                setMapLocationForAddress(address: address)
+            } else {
+                let message = "No address found"
+                let alertController = UIAlertController(title: "Message", message: message, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(okAction)
+                present(alertController, animated: true, completion: nil)
+            }
         }
     }
 
-    // MARK: Custom methods
-    func addressOf(_ account: Client) -> String {
-        var addressString = ""
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
+        // let us save the location
+        let center = mapView.region.center
+        let dictionary = ["lat": center.latitude,
+                          "lng": center.longitude]
+        UserDefaults.standard.set(dictionary, forKey: "lastLocation")
+        UserDefaults.standard.synchronize()
+    }
+    
+    // MARK: Custom methods
+    
+    /*
+     * Get the address of client
+     */
+    func addressOf(_ account: Client) -> String? {
         if let addresses = account.addresses {
             if let addressArray = NSKeyedUnarchiver.unarchiveObject(with: addresses as Data) as? [[String: Any]] {
                 for a in addressArray {
                     if let type = a["type"] as? String {
-                        if type == "Visit" {
-                            addressString += "Visit Address: "
-                        } else if type == "Mail" {
-                            addressString += "Postal Address: "
-                        }
-                        
-                        if let address = a["address"] as? String {
-                            addressString += "\(address)"
-                        }
-                        if let city = a["city"] as? String {
-                            addressString += ", \(city)"
-                        }
-                        if let state = a["state"] as? String {
-                            addressString += ", \(state)"
-                        }
-                        if let country = a["country"] as? String {
-                            if let countryName = Locale.current.localizedString(forRegionCode: country) {
-                                addressString += ", \(countryName)"
+                        if type == "Visit" || type == "Mail" {
+                            var addressString = ""
+                            
+                            if let address = a["address"] as? String {
+                                addressString += "\(address)"
                             }
+                            if let city = a["city"] as? String {
+                                if addressString.characters.count > 0 {
+                                    addressString += ", "
+                                }
+                                addressString += "\(city)"
+                            }
+                            if let state = a["state"] as? String {
+                                if addressString.characters.count > 0 {
+                                    addressString += ", "
+                                }
+                                addressString += "\(state)"
+                            }
+                            if let country = a["country"] as? String {
+                                if let countryName = Locale.current.localizedString(forRegionCode: country) {
+                                    if addressString.characters.count > 0 {
+                                        addressString += ", "
+                                    }
+                                    addressString += "\(countryName)"
+                                }
+                            }
+                            if let zipcode = a["zipcode"] as? String {
+                                if addressString.characters.count > 0 {
+                                    addressString += " "
+                                }
+                                addressString += "\(zipcode)"
+                            }
+                            
+                            return addressString
                         }
-                        if let zipcode = a["zipcode"] as? String {
-                            addressString += " \(zipcode)"
-                        }
-                        break
                     }
                 }
             }
             
         }
         
-        return addressString
+        return nil
     }
     
+    /*
+     * Geocode the address string and put it into the map
+     */
     func setMapLocationForAddress(address: String) {
         let geocoder:CLGeocoder = CLGeocoder()
         
         geocoder.geocodeAddressString(address, completionHandler: { (placemarks: [CLPlacemark]?, error: Error?) in
             if let error = error {
-                let alertController = UIAlertController(title: "Error", message: "\(error)", preferredStyle: .alert)
+                let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
                 let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
                 alertController.addAction(okAction)
                 self.present(alertController, animated: true, completion: nil)
@@ -89,12 +131,15 @@ class AccountLocationViewController: UIViewController {
                         if let topResult = placemarks.first {
                             let placemark = MKPlacemark(placemark: topResult)
                             
-                            var region: MKCoordinateRegion = self.mapView.region
-                            region.center = (placemark.location?.coordinate)!
-                            region.span.longitudeDelta /= 5.0
-                            region.span.latitudeDelta /= 5.0
+                            let coordinate = placemark.location?.coordinate
+                            var region = self.mapView.region
+                            region.center = coordinate!
+                            region.span.longitudeDelta /= 10.0
+                            region.span.latitudeDelta /= 10.0
+                            
                             self.mapView.setRegion(region, animated: true)
                             self.mapView.addAnnotation(placemark)
+                            self.mapView.selectAnnotation(placemark, animated: true)
                         }
                     }
                 }
