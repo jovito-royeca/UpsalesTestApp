3b4700caa0d97f3897aa26e37cb71d027f778f1e
Jovito Royeca <jovit.royeca@gmail.com>
2017-02-28 10:28:09 +0800

    Can now show account location with Map view

--- UpsalesTestApp/AccountLocationViewController.swift
+++ UpsalesTestApp/AccountLocationViewController.swift
@@ -10,22 +10,97 @@ import UIKit
 import MapKit
 
 class AccountLocationViewController: UIViewController {
-    // MARK: Outlets
+    // MARK: Variables
+    var account:Client?
     
+    // MARK: Outlets
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
-    
-    
     
     // MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let account = account {
+            let address = addressOf(account)
+            
+            nameLabel.text = account.name
+            addressLabel.text = address
+            setMapLocationForAddress(address: address)
+        }
     }
 
+    // MARK: Custom methods
+    func addressOf(_ account: Client) -> String {
+        var addressString = ""
+        
+        if let addresses = account.addresses {
+            if let addressArray = NSKeyedUnarchiver.unarchiveObject(with: addresses as Data) as? [[String: Any]] {
+                for a in addressArray {
+                    if let type = a["type"] as? String {
+                        if type == "Visit" {
+                            addressString += "Visit Address: "
+                        } else if type == "Mail" {
+                            addressString += "Postal Address: "
+                        }
+                        
+                        if let address = a["address"] as? String {
+                            addressString += "\(address)"
+                        }
+                        if let city = a["city"] as? String {
+                            addressString += ", \(city)"
+                        }
+                        if let state = a["state"] as? String {
+                            addressString += ", \(state)"
+                        }
+                        if let country = a["country"] as? String {
+                            if let countryName = Locale.current.localizedString(forRegionCode: country) {
+                                addressString += ", \(countryName)"
+                            }
+                        }
+                        if let zipcode = a["zipcode"] as? String {
+                            addressString += " \(zipcode)"
+                        }
+                        break
+                    }
+                }
+            }
+            
+        }
+        
+        return addressString
+    }
     
+    func setMapLocationForAddress(address: String) {
+        let geocoder:CLGeocoder = CLGeocoder()
+        
+        geocoder.geocodeAddressString(address, completionHandler: { (placemarks: [CLPlacemark]?, error: Error?) in
+            if let error = error {
+                let alertController = UIAlertController(title: "Error", message: "\(error)", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+            } else {
+                if let placemarks = placemarks {
+                    if placemarks.count > 0 {
+                        if let topResult = placemarks.first {
+                            let placemark = MKPlacemark(placemark: topResult)
+                            
+                            var region: MKCoordinateRegion = self.mapView.region
+                            region.center = (placemark.location?.coordinate)!
+                            region.span.longitudeDelta /= 5.0
+                            region.span.latitudeDelta /= 5.0
+                            self.mapView.setRegion(region, animated: true)
+                            self.mapView.addAnnotation(placemark)
+                        }
+                    }
+                }
+            }
+        })
+    }
 
 }
 
--- UpsalesTestApp/AccountsViewController.swift
+++ UpsalesTestApp/AccountsViewController.swift
@@ -23,8 +23,8 @@ class AccountsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let hud = MBProgressHUD.showAdded(to: view, animated: true)
-        UpsalesAPI.sharedInstance.fetchAccounts({ error in
+        MBProgressHUD.showAdded(to: view, animated: true)
+        UpsalesAPI.sharedInstance.fetchAccounts(completion: { error in
             DispatchQueue.main.async {
                 MBProgressHUD.hide(for: self.view, animated: true)
                 self.dataSource = self.getDataSource(nil)
@@ -33,6 +33,20 @@ class AccountsViewController: UIViewController {
         })
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showAccountLocation"?:
+            if let dest = segue.destination as? AccountLocationViewController,
+                let account = sender as? Client {
+                
+                dest.account = account
+            }
+        default:
+            ()
+        }
+    }
+    
+    // MARK: CUstom methods
     func getDataSource(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>?) -> DATASource? {
         var request:NSFetchRequest<NSFetchRequestResult>?
         if let fetchRequest = fetchRequest {
@@ -42,9 +56,8 @@ class AccountsViewController: UIViewController {
             request!.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
         }
         
-        let dataSource = DATASource(tableView: tableView, cellIdentifier: "Cell", fetchRequest: request!, mainContext: API.sharedInstance.dataStack.mainContext, sectionName: nil, configuration: { cell, item, indexPath in
-            if let account = item as? Client,
-                let cell = cell as? UITableViewCell {
+        let dataSource = DATASource(tableView: tableView, cellIdentifier: "Cell", fetchRequest: request!, mainContext: UpsalesAPI.sharedInstance.dataStack.mainContext, sectionName: nil, configuration: { cell, item, indexPath in
+            if let account = item as? Client {
                 
                 self.configureCell(cell, withAccount: account)
             }
@@ -55,12 +68,26 @@ class AccountsViewController: UIViewController {
         return dataSource
     }
     
-    func configureCell(_ cell: UITableViewCell, account: Client) {
-        cell.textLabel.text = account.name
-        
+    func configureCell(_ cell: UITableViewCell, withAccount account: Client) {
+        cell.textLabel?.text = account.name
+        cell.detailTextLabel?.text = account.webpage
     }
 }
 
+// MARK: DATASourceDelegate
+extension AccountsViewController: DATASourceDelegate {
+    func sectionIndexTitlesForDataSource(_ dataSource: DATASource, tableView: UITableView) -> [String] {
+        // return an empty String array to remove the section indexes
+        return [String]()
+    }
+}
+
+
+// MARK: UITableViewDelegate
 extension AccountsViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let accounts = dataSource!.all()
+        let account = accounts[indexPath.row]
+        performSegue(withIdentifier: "showAccountLocation", sender: account)
+    }
 }
